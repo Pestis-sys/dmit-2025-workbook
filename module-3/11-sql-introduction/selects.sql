2ce1c9da34304e1d78d404dbc3196f5e3bf7fe5c
Pestis-sys <avrice99@hotmail.com>
2026-02-12 09:47:08 -0700

    classword

---
+++ module-3/11-sql-introduction/selects.sql
@@ -0,0 +1,10 @@
+SELECT city_name`city_name` FROM `cities`;
+-- returs the city names in out quires 
+
+SELECT * FROM `cities` limit 3;
+-- only returns 3 results 
+
+Select * from  `cities` Where CID = 4;
+-- returns all info for the city owith an id of 4 
+
+Select * from `cities` WHERE province = 'mb';
